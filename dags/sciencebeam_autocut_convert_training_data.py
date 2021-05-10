9677a561611b3128747de91e3d700fe9ce6d26fb
Daniel Ecer <de-code@users.noreply.github.com>
2021-05-10 14:10:38 +0100

    added dags init py

--- dags/sciencebeam_autocut_convert_training_data.py
+++ dags/sciencebeam_autocut_convert_training_data.py
@@ -8,7 +8,7 @@ from sciencebeam_airflow.dags.utils import (
     get_default_args
 )
 
-from sciencebeam_convert import (
+from dags.sciencebeam_convert import (
     create_dag as create_sciencebeam_convert_dag,
     ScienceBeamConvertMacros
 )
